53f9167bdd400e83ac38bd968b146c5a8811659f
JhoanSebastianGelvesHernandezADSO <jhoangelves1205@gmail.com>
2023-03-14 17:25:34 -0500

    test: :art: v-model
    manipulando el v-model

--- componentes/my_component.js
+++ componentes/my_component.js
@@ -2,9 +2,7 @@ export  default{
 
     data(){
         return{
-            Basico: `<strong style="color:green;">Apolo</strong>`,
-            Intermedio:`<strong style="color:orange;">Artemis</strong>`,
-            Avanzado:`<strong style="color:blue;">sputnik</strong>`
+            mensaje: "lorem"
         }
     }
 }
