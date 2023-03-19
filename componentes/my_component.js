9661e29ee56d1178adbe98d5b9bfb0a62dfb2dad
JhoanSebastianGelvesHernandezADSO <jhoangelves1205@gmail.com>
2023-03-19 18:58:52 -0500

    v-for

--- componentes/my_component.js
+++ componentes/my_component.js
@@ -2,11 +2,11 @@ export  default{
 
     data(){
         return{
-            mensaje: `<em>Apolo</em>`,
-            mensaje2: `<mark>Artemis</mark>`,
-            mensaje3: `<u>Sputnik</u>`,
-            mensaje4: `No cumple la condicion`,
-            opcion:null
+            lista:["Apolo",
+                "Artemis",
+                "Sputnik",
+                ]
+            
         }
     }
 }
