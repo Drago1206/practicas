563220a6a9455c8868f93c359e3f89a3be125fee
JhoanSebastianGelvesHernandezADSO <jhoangelves1205@gmail.com>
2023-03-19 19:41:43 -0500

    v-objeto

--- componentes/my_component.js
+++ componentes/my_component.js
@@ -2,11 +2,10 @@ export  default{
 
     data(){
         return{
-            atributo: "src",
-            imagen:"Screenshot_48.png",
-            texto:"Diferencias entre V2 y V3",
-            atributo2:"alt",
-            texto:"Diferencias entre vue2 y vue3"
-        }   
+            imagen:{
+            src:"Screenshot_48.png",
+            title:"Diferencias entre V2 y V3"
+            }  
+        }    
     }
 }
