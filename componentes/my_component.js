0a2069882754432e8f1973897a7707fe7ebe099f
JhoanSebastianGelvesHernandezADSO <jhoangelves1205@gmail.com>
2023-03-19 19:38:16 -0500

    Dinamico

--- componentes/my_component.js
+++ componentes/my_component.js
@@ -2,8 +2,11 @@ export  default{
 
     data(){
         return{
+            atributo: "src",
             imagen:"Screenshot_48.png",
-            texto:"Diferencias entre V2 y V3"
-        }
+            texto:"Diferencias entre V2 y V3",
+            atributo2:"alt",
+            texto:"Diferencias entre vue2 y vue3"
+        }   
     }
 }
